fa578a1c96f05b9f54c2124f2b6acd6ff68661c9
Vinesse <vverning@gmail.com>
2021-08-26 15:54:45 +0200

    primitives and inputs

---
+++ FirstProgram/Primitives.java
@@ -0,0 +1,10 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 43829483.432493;
+        long largeIntegerValues = 2342342566452343L;
+        boolean check = true;
+    }
+}
